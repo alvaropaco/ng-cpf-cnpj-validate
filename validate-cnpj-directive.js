18cb80efb53e9b6673ec648cb68f102cebb799bf
alvaropaco <alvaropaco@tokenlab.com.br>
2015-05-21 18:52:36 -0300

    First commit#v1.0.0

---
+++ validate-cnpj-directive.js
@@ -0,0 +1,120 @@
+/**
+ * @author Alvaro Paçó <alvaro@scalasoft.com.br>
+ */
+(function () {
+  'use strict';
+
+  angular.module('validate-cnpj', [])
+
+  /**
+   * Validate CNPJ directive
+   * @param {ngModel} model The Model to bind
+   * @returns {boolean/'undefined'}
+   */
+    .directive('validateCnpj', function ($compile) {
+      return {
+        require: 'ngModel',
+        scope: { 'validateCnpj': '=' },
+        link: function (scope, element, attrs, ctrl) {
+          element.bind("blur", function () {
+            //valida o cpf
+            ctrl.$setValidity('validateCnpj', validaCNPJ(ctrl.$modelValue));
+
+            if (ctrl.$isEmpty(ctrl.$modelValue)) {
+              // consider empty models to be valid
+              return true;
+            }
+
+            //remove error-message
+            element.parent().removeClass('ng-invalid alert-danger');
+            element.parent().find('.alert-status-invalid').remove();
+            //if value invalido
+            if (ctrl.$dirty && ctrl.$invalid) {
+              //add error-message
+              element.parent().addClass('has-error');
+              element.parent().append(' <label class=" alert-status-invalid ' +
+                'control-label" for="inputError1">CNPJ Inválido</label>');
+              // Needle to work
+              return undefined;
+            } else {
+              //if success remove error-message
+              element.parent().removeClass('ng-invalid alert-danger');
+              element.parent().find('.alert-status-invalid').remove();
+              // Return the same value to work with ui-mask
+              // because if return another value, the ui-mask
+              // will be called again and validate too.
+              return ctrl.$viewValue;
+            }
+          });
+
+          /**
+           * Calculate validity of string
+           * @param str
+           * @returns {boolean}
+           */
+          var validaCNPJ = function (str) {
+            if (str == null)
+              return false;
+
+            str = str.replace(/\./g, '');
+            str = str.replace('/', '');
+            str = str.replace('-', '');
+
+            var cnpj = str;
+            var tamanho;
+            var numeros;
+            var digitos;
+            var soma;
+            var pos;
+            var resultado;
+            var i;
+
+            if (cnpj == '')
+              return false;
+
+            if (cnpj.length != 14)
+              return false;
+
+            // Regex to validate strings with 14 same characters
+            var regex = /([0]{14}|[1]{14}|[2]{14}|[3]{14}|[4]{14}|[5]{14}|[6]{14}|[7]{14}|[8]{14}|[9]{14})/g
+            // Regex builder
+            var patt = new RegExp(regex);
+            if (patt.test(cnpj))
+              return false;
+
+            // Valida DVs
+            tamanho = cnpj.length - 2
+            numeros = cnpj.substring(0, tamanho);
+            digitos = cnpj.substring(tamanho);
+            soma = 0;
+            pos = tamanho - 7;
+            for (i = tamanho; i >= 1; i--) {
+              soma += numeros.charAt(tamanho - i) * pos--;
+              if (pos < 2)
+                pos = 9;
+            }
+            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+            if (resultado != digitos.charAt(0))
+              return false;
+
+            tamanho = tamanho + 1;
+            numeros = cnpj.substring(0, tamanho);
+            soma = 0;
+            pos = tamanho - 7;
+            for (i = tamanho; i >= 1; i--) {
+              soma += numeros.charAt(tamanho - i) * pos--;
+              if (pos < 2)
+                pos = 9;
+            }
+            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+            if (resultado != digitos.charAt(1))
+              return false;
+
+            return true;
+          }
+
+        }
+      };
+    });
+
+}).call(this);
---
+++ validate-cpf-directive.js
@@ -0,0 +1,93 @@
+/**
+ * @author Alvaro Paçó <alvaro@scalasoft.com.br>
+ */
+(function () {
+  'use strict';
+
+  angular.module('validate-cpf', [])
+  /**
+   * Validate CPF directive
+   * @param {ngModel} model The Model to bind
+   * @returns {boolean/'undefined'}
+   */
+    .directive('validateCpf', function () {
+      return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, ctrl) {
+          element.bind("blur", function () {
+            //valida o cpf
+            ctrl.$setValidity('validateCpf', validaCPF(ctrl.$modelValue));
+            scope.$apply();
+
+            //remove error-message
+            element.parent().removeClass('ng-invalid alert-danger');
+            element.parent().find('.alert-status-invalid').remove();
+            //if cpf invalido
+            if (ctrl.$dirty && ctrl.$invalid) {
+              element.parent().addClass('has-error');
+              element.parent().append(' <label class=" alert-status-invalid ' +
+                'control-label" for="inputError1">CPF Inválido</label>');
+              // Setting $valid to false
+              ctrl.$setValidity('validateCpf', false);
+              // Needle to work
+              return undefined;
+            } else {
+              //if success remove error-message
+              element.parent().removeClass('has-error');
+              element.parent().find('.alert-status-invalid').remove();
+              // Return the same value to work with ui-mask
+              // because if return another value, the ui-mask
+              // will be called again and validate too.
+              return ctrl.$viewValue;
+            }
+          });
+
+          /**
+           * Calculate validity of string
+           * @param {string} str
+           * @returns {boolean}
+           */
+          function validaCPF(str) {
+            if (str) {
+              str = str.replace('.', '');
+              str = str.replace('.', '');
+              str = str.replace('-', '');
+
+              var cpf = str;
+              var numeros, digitos, soma, i, resultado, digitos_iguais;
+              digitos_iguais = 1;
+              if (cpf.length < 11)
+                return false;
+              for (i = 0; i < cpf.length - 1; i++)
+                if (cpf.charAt(i) != cpf.charAt(i + 1)) {
+                  digitos_iguais = 0;
+                  break;
+                }
+              if (!digitos_iguais) {
+                numeros = cpf.substring(0, 9);
+                digitos = cpf.substring(9);
+                soma = 0;
+                for (i = 10; i > 1; i--)
+                  soma += numeros.charAt(10 - i) * i;
+                resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+                if (resultado != digitos.charAt(0))
+                  return false;
+                numeros = cpf.substring(0, 10);
+                soma = 0;
+                for (i = 11; i > 1; i--)
+                  soma += numeros.charAt(11 - i) * i;
+                resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+                if (resultado != digitos.charAt(1))
+                  return false;
+                return true;
+              }
+              else
+                return false;
+            }
+          }
+
+        }
+      };
+    });
+
+}).call(this);
